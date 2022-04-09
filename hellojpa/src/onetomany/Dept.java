56581ecb5216354716e062ad3a3d44184f959b3b
skskdmsdl <skskdmsdl@gmail.com>
2022-04-09 15:37:04 +0900

    one to many 관계

---
+++ hellojpa/src/onetomany/Dept.java
@@ -0,0 +1,32 @@
+package onetomany;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+//@Entity
+@Table(name="departments")
+public class Dept {
+
+	@Id
+	@Column(name="department_id")
+	Integer departmentId;
+	
+	@Column(name="department_name")
+	String departmentName;
+	
+	@OneToMany
+	@JoinColumn(name="department_id", referencedColumnName = "department_id")
+	List<Emp> list;
+
+	@Override
+	public String toString() {
+		return "Dept [departmentId=" + departmentId + ", departmentName=" + departmentName + "]";
+	}
+	
+}
